b850262fd16633c29c11aaf74ced5317cad7a323
Aleksandar <aleksandarjjj@icloud.com>
2020-10-15 12:33:57 +0200

    moved store to separate js file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
 
 import App from './App.vue'
 import router from './router.js'
+import store from './store.js'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch, faPlusCircle, faArrowLeft, faMinusCircle } from '@fortawesome/free-solid-svg-icons'
@@ -14,47 +14,6 @@ import 'bootstrap'
 import './assets/scss/custom.scss'
 
 const app = createApp(App)
-const store = createStore({
-    state() {
-        return {
-            targets: []
-        }
-    },
-    getters: {
-        getTargets: (state) => {
-            return state.targets
-        },
-        getTarget: (_, getters) => (id) => {
-            return getters.getTargets.find(
-                (target) => target.id === id
-            );
-        }
-    },
-    mutations: {
-        addTarget(state, target) {
-            state.targets.unshift(target)
-        },
-        setTargets(state, targets) {
-            state.targets = targets
-        },
-        deleteTarget(state, id) {
-            state.targets = state.targets.filter(
-                (target) => target.id !== id
-            );
-        }
-    },
-    actions: {
-        addTarget({commit}, target) {
-            commit('addTarget', target)
-        },
-        setTargets({commit}, targets) {
-            commit('setTargets', targets)
-        },
-        deleteTarget({commit}, id) {
-            commit('deleteTarget', id)
-        }
-    }
-})
 
 app.use(router)
 app.use(store)
