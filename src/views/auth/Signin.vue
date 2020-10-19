d433eb29acbb8c3c93b0fcfa503de97cd40cc478
Aleksandar <aleksandarjjj@icloud.com>
2020-10-19 14:51:48 +0200

    fixed signin width

--- src/views/auth/Signin.vue
+++ src/views/auth/Signin.vue
@@ -1,76 +1,86 @@
 <template>
-  <template v-if="!showCompleteSignin">
-    <div class="row mt-5">
-      <div class="col">
-        <form @submit.prevent="signIn">
-          <div class="row mb-2">
-            <div class="col">
-              <input
-                id="email"
-                type="email"
-                placeholder="Email"
-                class="form-control form-control-lg"
-                required
-                ref="email"
-              />
-            </div>
+  <div class="row">
+    <div class="col-md-8 offset-md-2">
+      <template v-if="!showCompleteSignin">
+        <div class="row mt-5">
+          <div class="col">
+            <form @submit.prevent="signIn">
+              <div class="row mb-2">
+                <div class="col">
+                  <input
+                    id="email"
+                    type="email"
+                    placeholder="Email"
+                    class="form-control form-control-lg"
+                    required
+                    ref="email"
+                  />
+                </div>
+              </div>
+              <div class="row mb-2">
+                <div class="col">
+                  <input
+                    id="password"
+                    type="password"
+                    placeholder="Password"
+                    class="form-control form-control-lg"
+                    required
+                    ref="password"
+                  />
+                </div>
+              </div>
+              <div class="row mb-2">
+                <div class="col">
+                  <button
+                    type="submit"
+                    class="btn btn-block btn-lg btn-primary"
+                  >
+                    {{ signingIn ? "Signing in..." : "Sign in" }}
+                  </button>
+                </div>
+              </div>
+              <div class="row text-center">
+                <div class="col">
+                  <a href="#">Forgot Password?</a>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="row mb-2">
-            <div class="col">
-              <input
-                id="password"
-                type="password"
-                placeholder="Password"
-                class="form-control form-control-lg"
-                required
-                ref="password"
-              />
-            </div>
+        </div>
+      </template>
+      <template v-else>
+        <div class="row mt-5">
+          <div class="col">
+            <p class="mb-2">Please enter a new password.</p>
           </div>
-          <div class="row mb-2">
-            <div class="col">
-              <button type="submit" class="btn btn-block btn-lg btn-primary">
-                {{signingIn ? 'Signing in...':'Sign in'}}
-              </button>
-            </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <form @submit.prevent="completeSignin">
+              <div class="row mb-2">
+                <div class="col">
+                  <password-input
+                    placeholder="New password"
+                    ref="newPassword"
+                  ></password-input>
+                </div>
+              </div>
+              <div class="row mb-2">
+                <div class="col">
+                  <button
+                    type="submit"
+                    class="btn btn-block btn-lg btn-primary"
+                  >
+                    Confirm
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="row text-center">
-            <div class="col">
-              <a href="#">Forgot Password?</a>
-            </div>
-          </div>
-        </form>
-      </div>
+        </div>
+      </template>
     </div>
-  </template>
-  <template v-else>
-    <div class="row mt-5">
-      <div class="col">
-        <p class="mb-2">Please enter a new password.</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <form @submit.prevent="completeSignin">
-          <div class="row mb-2">
-            <div class="col">
-              <password-input
-                placeholder="New password"
-                ref="newPassword"
-              ></password-input>
-            </div>
-          </div>
-          <div class="row mb-2">
-            <div class="col">
-              <button type="submit" class="btn btn-block btn-lg btn-primary">
-                Confirm
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </template>
+  </div>
 </template>
 
 <script>
@@ -96,7 +106,6 @@ export default {
       const password = this.$refs.password.value;
 
       const user = await Auth.signIn(email, password);
-      console.log(user)
       if (user.challengeName === "NEW_PASSWORD_REQUIRED") {
         this.showCompleteSignin = true;
         this.user = user;
