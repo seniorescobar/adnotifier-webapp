e3a98b488f29ac87c7ace91ed073309a17b869b3
Aleksandar Jelić <aleksandar@jelic.email>
2020-11-06 14:55:02 +0100

    ViewTarget => composition api

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -13,3 +13,38 @@ export async function fetchTargets() {
         }
     ).then((r) => r.json());
 }
+
+export async function fetchTarget(id) {
+    const sess = await Auth.currentSession();
+    const token = sess.getIdToken().getJwtToken();
+
+    return fetch(
+        "https://rftdwuwyj4.execute-api.eu-central-1.amazonaws.com/dev/target/" +
+        id,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token,
+            },
+        }
+    ).then((r) => r.json());
+}
+
+export async function deleteTarget(id) {
+    const sess = await Auth.currentSession();
+    const token = sess.getIdToken().getJwtToken();
+
+    fetch(
+        "https://rftdwuwyj4.execute-api.eu-central-1.amazonaws.com/dev/target/" +
+        id,
+        {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token,
+            },
+        }
+    )
+        .then((response) => response.json())
+}
